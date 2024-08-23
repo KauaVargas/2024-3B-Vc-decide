13f2554a7e827ee27d464a395a020371a9a52b5a
Kauã Vinícius <106679339+KauaVargas@users.noreply.github.com>
2024-08-23 00:39:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,12 +9,12 @@ const perguntas = [
         enunciado: "Antonio é um garoto de 5 anos que está enfrentando dificuldades em sua escola por estar sofrendo racismo devido a cor de sua pele negra, sua mãe ficou sabendo do ocorrido e foi resolver. O que ela deveria fazer?",
         alternativas: [
             {
-                texto:"Conversar com a diretora para solucionar o problema",
-                afirmacao: "A mãe de Antonio foi na escola para resolver um caso de racismo com seu filho"
+                texto:"Conversar com a diretora para solucionar o problema.",
+                afirmacao: "A mãe de Antonio, um garoto de 5 anos, foi na escola para resolver um caso de racismo com seu filho, juntamente com a diretora, eles conseguem solucionar o problema. "
             },
             {
                 texto:"Retirar Antonio da escola e fazer uma denuncia.",
-                afirmacao: "A mãe de Antonio retirou seu filho da escola devido ao um caso de racismo e fez uma denuncia"
+                afirmacao: "A mãe de Antonio, um garoto de 5 anos, foi na escola resolver um caso de racismo com seu filho, então ela acaba optando por retirar ele da escola."
             }   
         ]
     },
@@ -23,11 +23,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Exigir a demissão dos responsáveis pelos insultos.",
-                afirmacao: "Antonio se revolta e exige a demissão dos responsáveis pelos insultos"
+                afirmacao: "Quando Antonio cresceu, ele sofreu do mesmo problema no trabalho, então Antonio se revolta e exige a demissão dos responsáveis pelos insultos."
             },
             {
                 texto: "Ignorar a situação para não arrumar problemas.",
-                afirmacao: "Antonio ignora o problema com medo de ser demitido"
+                afirmacao: "Quando Antonio cresceu, ele sofreu do mesmo problema no trabalho, mas Antonio resolveu ignorar o problema com medo de ser demitido."
             }
         ]
     },
@@ -36,11 +36,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Estudar e montar artigos falando sobre esse tema para ajudar sua comunidade.",
-                afirmacao: "Com o intuito de ajudar sua comunidade, Antonio estuda sobre o tema de racismo e monta artigos para mostrar a situação que muitos passam"
+                afirmacao: "Com o intuito de ajudar sua comunidade, Antonio estuda sobre o tema de racismo e monta artigos para mostrar a situação que muitos passam."
             },
             {
                 texto: "Não deve dar atenção para isso, pois já está no passado.",
-                afirmacao: "Antonio pensa melhor e decide deixar isso no passado"
+                afirmacao: "Após um tempo, Antonio reflete sobre os ataques de racismo e pensa em tomar uma atitude, porém Antonio pensa melhor e decide deixar isso no passado."
             }
         ]
     },
@@ -49,11 +49,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Aceitar a boa vontade dos colégas e entrar no negócio.",
-                afirmacao: "Ele aceita e entra para o novo negócio"
+                afirmacao: "Anos depois, antigos colegas reencontram Antonio e reconhecem sua superação de vida, então acabam optando por convida-lo para um negócio de empreendedorismo, ele aceita e entra para o novo negócio."
             },
             {
                 texto: "Negar e seguir o rumo sozinho, como sempre fez.",
-                afirmacao: "Ele agradece, porém recusa e segue o rumo sozinho"
+                afirmacao: "Anos depois, antigos colegas reencontram Antonio e reconhecem sua superação de vida, então acabam optando por convida-lo para um negócio de empreendedorismo, ele agradece, porém recusa e segue o rumo sozinho."
             }
         ]
     },
@@ -62,11 +62,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Investir mais em projetos de caridade, ajudando sua comunidade como sempre sonhou.",
-                afirmacao: "Antonio investe em projetos de caridade e fica conhecido como um herói de sua comunidade"
+                afirmacao: "Antonio consegue se tornar um homem de sucesso e ganha muito reconhecimento com projetos solidários, então Antonio resolve investir ainda mais em projetos de caridade e fica conhecido como um herói de sua comunidade."
             },
             {
                 texto: "Se aposentar e aproveitar como um verdadeiro vencedor da vida",
-                afirmacao: "Antonio decide se aposentar e aproveitar o resto da vida como um vencedor"
+                afirmacao: "Antonio consegue se tornar um homem de sucesso e ganha muito reconhecimento com projetos solidários, então Antonio decide se aposentar e aproveitar o resto da vida como um vencedor."
             }
         ]
     },
